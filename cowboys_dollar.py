b051a7afb676fcf9aa9ac5846c8473454656f1a2
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-04-29 23:02:29 -0600

    Solucion larga

--- cowboys_dollar.py
+++ cowboys_dollar.py
@@ -2,18 +2,30 @@ def cowboys_dollars(boots):
     boot = 0
     counter_left = 0
     counter_right = 0
+    aux = ""
     for i in boots:
         for j in i:
-            if j == "1" and boot == 0:
+            
+            if j == "(" and aux == "":
+                aux += "("
+            elif j == "1" and aux == "(":
+                aux += "1"
+            elif j == ")" and aux == "(1" and boot == 0:
                 counter_left += 1
                 print("billete detectado en izquierda")
-            elif j == "1" and boot == 1:
+                aux = ""
+            elif j == ")" and aux == "(1" and boot == 1:
                 counter_right += 1
                 print("billete detectado en derecha")
+                aux = ""
             elif j == "&":
+                aux = ""
                 break
+            else:
+                aux = ""
+            
         boot += 1
     print(counter_left)
     print(counter_right)
     
-    return "This Rhinestone Cowboy has {} dollar bills in his right boot and {} in the left".format(counter_left, counter_right)
+    return "This Rhinestone Cowboy has {} dollar bills in his right boot and {} in the left".format(counter_right, counter_left)
